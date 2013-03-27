1e70fdbf48b2c160dc801c46586cbebcbf3760c4
Mohammed Hassan <mohammed.hassan@jollamobile.com>
2013-03-27 15:03:18 +0200

    Register all our elemeents

--- gst/codecbin/plugin.c
+++ gst/codecbin/plugin.c
@@ -8,13 +8,22 @@
 
 #include <gst/gst.h>
 #include "gstmpeg4vdec.h"
+#include "gstmpeg2vdec.h"
+#include "gstvc1vdec.h"
+#include "gsth263dec.h"
 
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
   return
       gst_element_register (plugin, "mpeg4vdec", GST_RANK_PRIMARY + 10,
-      gst_mpeg4v_dec_get_type ());
+      gst_mpeg4v_dec_get_type ()) &&
+      gst_element_register (plugin, "mpeg2vdec", GST_RANK_PRIMARY + 10,
+      gst_mpeg2v_dec_get_type ()) &&
+      gst_element_register (plugin, "vc1vdec", GST_RANK_PRIMARY + 10,
+      gst_vc1v_dec_get_type ()) &&
+      gst_element_register (plugin, "h263dec", GST_RANK_PRIMARY + 10,
+      gst_h263_dec_get_type ());
 }
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR, "codecbin",
